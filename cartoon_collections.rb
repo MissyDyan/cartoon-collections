4a368d790d7c0bf2062b90601cb58ea9bf8cea84
Diane Nguyen <diane.nguyen@gmail.com>
2021-08-05 10:38:44 -0700

    Cartoon Collections assignment completed.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+#   # Your code here
+  dwarves.each.with_index(1) { |dwarf, index|
+    puts "#{index}. #{dwarf}" 
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.map { |element| "#{element}" 
+    element.capitalize + "!" 
+  }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
-end
+  calls.any? { |call| call.length > 4 }
+end 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)# code an argument here
+#   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if cheese.include?("cheddar")
+    cheese[1]
+  else
+    nil
+  end
 end
